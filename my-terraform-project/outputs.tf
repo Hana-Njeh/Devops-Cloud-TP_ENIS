7b6950d471c69dc71c4f898bf1524c7047fcd974
Hana-Njeh <118611041+Hana-Njeh@users.noreply.github.com>
2024-12-14 10:49:46 +0100

    Update outputs.t

--- my-terraform-project/outputs.tf
+++ my-terraform-project/outputs.tf
@@ -1,17 +1,18 @@
 output "instance_details" {
-description = "Detailed information about the EC2 instance"
-value = {
-instance_id = aws_instance.public_instance.id
-instance_public_ip = aws_instance.public_instance.public_ip
-instance_private_ip = aws_instance.public_instance.private_ip
-instance_name = aws_instance.public_instance.tags["Name"]
-security_group_id = aws_instance.public_instance.vpc_security_group_ids
-# Security group attached to the instance
-subnet_id = aws_instance.public_instance.subnet_id
+  description = "Detailed information about the EC2 instance"
+  value = {
+    instance_id       = aws_instance.public_instance.id
+    instance_public_ip = aws_instance.public_instance.public_ip
+    instance_private_ip = aws_instance.public_instance.private_ip
+    instance_name      = aws_instance.public_instance.tags["Name"]
+    security_group_id  = aws_instance.public_instance.vpc_security_group_ids
+    subnet_id          = aws_instance.public_instance.subnet_id
+  }
 }
+
 output "deployer_key_s3_uri" {
   description = "S3 URI of the deployer key file"
-  value = "s3://${aws_s3_object.private_key_object.bucket}/${aws_s3_object.private_key_object.key}"
+  value       = "s3://${aws_s3_object.private_key_object.bucket}/${aws_s3_object.private_key_object.key}"
 }
 
 output "rds_endpoint" {
@@ -21,6 +22,4 @@ output "rds_endpoint" {
     username        = aws_db_instance.mydb.username
     db_instance_id  = aws_db_instance.mydb.id
   }
-}
-
 }
